23bf45eb85b17b9baf100b312ce4ed37d556fa16
SantiTV <79552393+SantiTV@users.noreply.github.com>
2023-05-16 09:25:45 -0500

    commit

---
+++ parcial2/src/app/cafes/cafe-detail/cafe-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import {}
+
+@Component({
+  selector: 'app-cafe-detail',
+  templateUrl: './cafe-detail.component.html',
+  styleUrls: ['./cafe-detail.component.css']
+})
+export class CafeDetailComponent {
+
+}
